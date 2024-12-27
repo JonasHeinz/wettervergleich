1da31f1f3c1401964584a70c12baef00e4488ed8
Jonas Heinz <64084505+JonasHeinz@users.noreply.github.com>
2024-12-27 17:05:01 +0100

    server2

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/py/:path*",
+        source: "/api/path*",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/py/:path*"
@@ -14,14 +14,14 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/py/docs"
-            : "/api/py/docs",
+            : "/api/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/py/openapi.json"
-            : "/api/py/openapi.json",
+            : "/api/openapi.json",
       },
     ];
   },
